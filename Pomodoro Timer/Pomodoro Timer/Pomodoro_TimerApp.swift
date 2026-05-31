e492f2c4c6becac08015b844c7bef04673c52631
Hans-Willem Moolman <hwmoolman@gmail.com>
2026-05-31 17:03:33 +0200

    Hello World

---
+++ Pomodoro Timer/Pomodoro Timer/Pomodoro_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pomodoro_TimerApp.swift
+//  Pomodoro Timer
+//
+//  Created by Hans-Willem Moolman on 2026/05/31.
+//
+
+import SwiftUI
+
+@main
+struct Pomodoro_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
